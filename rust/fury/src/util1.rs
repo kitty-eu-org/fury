8edff920d3654fa63d0be50d579aa576edd8e065
hezz <hezhaozhaog@gmail.com>
2024-07-24 23:18:53 +0800

    feat: add avx && sse2 simd support

--- rust/fury/src/util1.rs
+++ rust/fury/src/util1.rs
@@ -25,7 +25,13 @@ use std::arch::aarch64::*;
     target_arch = "x86_64",
     all(target_arch = "aarch64", target_feature = "neon")
 ))]
-pub(crate) const MIN_DIM_SIZE_SIMD: usize = 8;
+pub(crate) const MIN_TO_UTF8_DIM_SIMD: usize = 8;
+
+#[cfg(target_arch = "x86_64")]
+pub(crate) const MIN_TO_UTF8_DIM_AVX: usize = 16;
+
+#[cfg(target_feature = "sse2")]
+use std::arch::x86_64::*;
 
 // Swapping the high 8 bits and the low 8 bits of a 16-bit value
 fn swap_endian(value: u16) -> u16 {
@@ -58,6 +64,148 @@ fn utf16_to_utf8(code_point: u16, offset: usize, ptr: *mut u8) -> usize {
     }
 }
 
+fn utf16_surrogate_pair_to_utf8(code_point: u16, offset: usize, ptr: *mut u8) -> usize {
+    // utf16 to unicode
+    let code_point =
+        ((((code_point as u32) - 0xd800) << 10) | ((code_point as u32) - 0xdc00)) + 0x10000;
+    // 11110??? 10?????? 10?????? 10??????
+    // Need 21 bit suffix of code_point
+    let bytes = [
+        (code_point >> 18 & 0b111) as u8 | 0b1111_0000,
+        (code_point >> 12 & 0b11_1111) as u8 | 0b1000_0000,
+        (code_point >> 6 & 0b11_1111) as u8 | 0b1000_0000,
+        (code_point & 0b11_1111) as u8 | 0b1000_0000,
+    ];
+    unsafe {
+        ptr::copy_nonoverlapping(bytes.as_ptr(), ptr.add(offset), 4);
+    }
+    4
+}
+
+#[cfg(target_arch = "x86_64")]
+unsafe fn to_utf8_avx(utf16: &[u16], is_little_endian: bool) -> Result<Vec<u8>, String> {
+    let utf16_len = utf16.len();
+    let mut offset = 0;
+    let mut utf8_bytes: Vec<u8> = Vec::with_capacity(utf16_len * 3);
+    let ptr = utf8_bytes.as_mut_ptr();
+    let limit1 = _mm256_set1_epi16(0x80);
+    let limit2 = _mm256_set1_epi16(0x800);
+    let surrogate_high_start = _mm256_set1_epi16(0xD800u16 as i16);
+    let surrogate_high_end = _mm256_set1_epi16(0xDBFFu16 as i16);
+    let surrogate_low_start = _mm256_set1_epi16(0xDC00u16 as i16);
+    let surrogate_low_end = _mm256_set1_epi16(0xDFFFu16 as i16);
+
+    let remaining = utf16_len % MIN_TO_UTF8_DIM_AVX;
+    let range_end = utf16_len - remaining;
+    for i in (0..range_end).step_by(MIN_TO_UTF8_DIM_AVX) {
+        let mut chunk = _mm256_loadu_si256(utf16.as_ptr().add(i) as *const __m256i);
+        if !is_little_endian {
+            chunk = _mm256_or_si256(_mm256_slli_epi16(chunk, 8), _mm256_srli_epi16(chunk, 8));
+            // Swap bytes for big-endian
+        }
+        let masked1 = _mm256_cmpgt_epi16(chunk, limit1);
+        let masked2 = _mm256_cmpgt_epi16(chunk, limit2);
+        let high_surrogate_masked = _mm256_and_si256(
+            _mm256_cmpgt_epi16(chunk, surrogate_high_start),
+            _mm256_cmpgt_epi16(chunk, surrogate_high_end),
+        );
+        let low_surrogate_masked = _mm256_and_si256(
+            _mm256_cmpgt_epi16(chunk, surrogate_low_start),
+            _mm256_cmpgt_epi16(chunk, surrogate_low_end),
+        );
+        if _mm256_testz_si256(masked1, masked1) == 1 {
+            for j in 0..MIN_TO_UTF8_DIM_AVX {
+                unsafe {
+                    ptr.add(offset).write(utf16[i + j] as u8);
+                }
+                offset += 1;
+            }
+            todo!();
+        } else if _mm256_testz_si256(masked2, masked2) == 1 {
+            for j in 0..MIN_TO_UTF8_DIM_AVX {
+                offset += utf16_to_utf8(utf16[i + j], offset, ptr)
+            }
+        } else {
+            for j in 0..MIN_TO_UTF8_DIM_AVX {
+                if _mm256_testz_si256(high_surrogate_masked, high_surrogate_masked) == 1
+                    && j + 1 < MIN_TO_UTF8_DIM_AVX
+                    && _mm256_testz_si256(low_surrogate_masked, low_surrogate_masked) == 1
+                {
+                    if !(0xdc00..=0xdfff).contains(&utf16[i + j]) {
+                        return Err("Invalid UTF-16 string: wrong surrogate pair".to_string());
+                    }
+                    offset += utf16_surrogate_pair_to_utf8(utf16[i + j], offset, ptr);
+                } else {
+                    offset += utf16_to_utf8(utf16[i + j], offset, ptr);
+                }
+            }
+        }
+    }
+    Ok(Vec::new())
+}
+
+#[cfg(target_feature = "sse2")]
+unsafe fn to_utf8_sse(utf16: &[u16], is_little_endian: bool) -> Result<Vec<u8>, String> {
+    let utf16_len = utf16.len();
+    let mut offset = 0;
+    let mut utf8_bytes: Vec<u8> = Vec::with_capacity(utf16_len * 3);
+    let ptr = utf8_bytes.as_mut_ptr();
+    let limit1 = _mm_set1_epi16(0x80);
+    let limit2 = _mm_set1_epi16(0x800);
+    let surrogate_high_start = _mm_set1_epi16(0xD800u16 as i16);
+    let surrogate_high_end = _mm_set1_epi16(0xDBFFu16 as i16);
+    let surrogate_low_start = _mm_set1_epi16(0xDC00u16 as i16);
+    let surrogate_low_end = _mm_set1_epi16(0xDFFFu16 as i16);
+
+    let remaining = utf16_len % MIN_TO_UTF8_DIM_SIMD;
+    let range_end = utf16_len - remaining;
+    for i in (0..range_end).step_by(MIN_TO_UTF8_DIM_SIMD) {
+        let mut chunk = _mm_loadu_si128(utf16.as_ptr().add(i) as *const __m128i);
+        if !is_little_endian {
+            chunk = _mm_or_si128(_mm_slli_epi16(chunk, 8), _mm_slli_epi16(chunk, 8));
+            // Swap bytes for big-endian
+        }
+        let masked1 = _mm_cmpgt_epi16(chunk, limit1);
+        let masked2 = _mm_cmpgt_epi16(chunk, limit2);
+        let high_surrogate_masked = _mm_and_si128(
+            _mm_cmpgt_epi16(chunk, surrogate_high_start),
+            _mm_cmpgt_epi16(chunk, surrogate_high_end),
+        );
+        let low_surrogate_masked = _mm_and_si128(
+            _mm_cmpgt_epi16(chunk, surrogate_low_start),
+            _mm_cmpgt_epi16(chunk, surrogate_low_end),
+        );
+        if _mm_testz_si128(masked1, masked1) == 1 {
+            for j in 0..MIN_TO_UTF8_DIM_SIMD {
+                unsafe {
+                    ptr.add(offset).write(utf16[i + j] as u8);
+                }
+                offset += 1;
+            }
+            todo!();
+        } else if _mm_testz_si128(masked2, masked2) == 1 {
+            for j in 0..MIN_TO_UTF8_DIM_SIMD {
+                offset += utf16_to_utf8(utf16[i + j], offset, ptr)
+            }
+        } else {
+            for j in 0..MIN_TO_UTF8_DIM_SIMD {
+                if _mm_testz_si128(high_surrogate_masked, high_surrogate_masked) == 1  // surrogate_high_start < chunk < surrogate_high_end
+                    && j + 1 < MIN_TO_UTF8_DIM_SIMD
+                    && _mm_testz_si128(low_surrogate_masked, low_surrogate_masked) == 0 // not surrogate_low_start < chunk < surrogate_low_end
+                {
+                    if !(0xdc00..=0xdfff).contains(&utf16[i + j]) {
+                        return Err("Invalid UTF-16 string: wrong surrogate pair".to_string());
+                    }
+                    offset += utf16_surrogate_pair_to_utf8(utf16[i + j], offset, ptr);
+                } else {
+                    offset += utf16_to_utf8(utf16[i + j], offset, ptr);
+                }
+            }
+        }
+    }
+    Ok(Vec::new())
+}
+
 #[cfg(target_feature = "neon")]
 unsafe fn to_utf8_neon(utf16: &[u16], is_little_endian: bool) -> Result<Vec<u8>, String> {
     let utf16_len = utf16.len();
@@ -71,9 +219,9 @@ unsafe fn to_utf8_neon(utf16: &[u16], is_little_endian: bool) -> Result<Vec<u8>,
     let surrogate_low_start = vdupq_n_u16(0xDC00);
     let surrogate_low_end = vdupq_n_u16(0xDFFF);
 
-    let remaining = utf16_len % MIN_DIM_SIZE_SIMD;
+    let remaining = utf16_len % MIN_TO_UTF8_DIM_SIMD;
     let range_end = utf16_len - remaining;
-    for i in (0..range_end).step_by(MIN_DIM_SIZE_SIMD) {
+    for i in (0..range_end).step_by(MIN_TO_UTF8_DIM_SIMD) {
         let mut chunk = vld1q_u16(utf16.as_ptr().add(i));
         if !is_little_endian {
             chunk = vorrq_u16(vshlq_n_u16(chunk, 8), vshrq_n_u16(chunk, 8)); // Swap bytes for big-endian
@@ -89,7 +237,7 @@ unsafe fn to_utf8_neon(utf16: &[u16], is_little_endian: bool) -> Result<Vec<u8>,
             vcltq_u16(chunk, surrogate_low_end),
         );
         if vmaxvq_u16(masked1) == 0 {
-            for j in 0..MIN_DIM_SIZE_SIMD {
+            for j in 0..MIN_TO_UTF8_DIM_SIMD {
                 unsafe {
                     ptr.add(offset).write(utf16[i + j] as u8);
                 }
@@ -97,16 +245,19 @@ unsafe fn to_utf8_neon(utf16: &[u16], is_little_endian: bool) -> Result<Vec<u8>,
             }
             todo!();
         } else if vmaxvq_u16(masked2) == 0 {
-            for j in 0..MIN_DIM_SIZE_SIMD {
+            for j in 0..MIN_TO_UTF8_DIM_SIMD {
                 offset += utf16_to_utf8(utf16[i + j], offset, ptr)
             }
         } else {
-            for j in 0..MIN_DIM_SIZE_SIMD {
+            for j in 0..MIN_TO_UTF8_DIM_SIMD {
                 if vmaxvq_u16(high_surrogate_masked) == 0
-                    && j + 1 < 8
+                    && j + 1 < MIN_TO_UTF8_DIM_SIMD
                     && vmaxvq_u16(low_surrogate_masked) != 0
                 {
-                    todo!();
+                    if !(0xdc00..=0xdfff).contains(utf16[i + j]) {
+                        return Err("Invalid UTF-16 string: wrong surrogate pair".to_string());
+                    }
+                    offset += utf16_surrogate_pair_to_utf8(utf16[i + j], offset, ptr);
                 } else {
                     offset += utf16_to_utf8(utf16[i + j], offset, ptr);
                 }
